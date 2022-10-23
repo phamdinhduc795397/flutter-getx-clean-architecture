39e4ba0c8c0d4990a9584dfec4c779536d4a521e
handiism <mail.handira@gmail.com>
2022-10-24 00:08:10 +0700

    refactor: rm unused import

--- lib/presentation/controllers/news/news_controller.dart
+++ lib/presentation/controllers/news/news_controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:getx_clean_architecture/domain/entities/article.dart';
 import 'package:getx_clean_architecture/domain/entities/paging.dart';
 import 'package:getx_clean_architecture/domain/usecases/fetch_news_use_case.dart';
--- lib/presentation/pages/headline/headline_page.dart
+++ lib/presentation/pages/headline/headline_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_clean_architecture/presentation/controllers/headline/headline_controller.dart';
 import 'package:getx_clean_architecture/presentation/pages/detail/detail_page.dart';
